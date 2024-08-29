30ad02ee28fbab0086474c6f563a61990585d43a
MrKuike <natanaelromilio@gmail.com>
2024-08-29 00:49:53 -0400

    descripcion añadida

--- client.js
+++ client.js
@@ -40,21 +40,38 @@ class appModule extends Module{
   }
 
   start(){
+    //////////////// CODIGO PARA UNA VENTANA//////////////
     this.milkyway = new Surface(
-      100,
-      100,
+      50,
+      50,
       Surface.SurfaceShape.Flat
   )
   
     this.milkyway.setAngle(0.01,-0.12)
   
     r360.renderToSurface(
-      r360.createRoot('infoPanel', { /* initial props */ }),
+      r360.createRoot('infoPanel', {
+        id: 'milkyway',
+        text: 'La via lactea es la galaxia en donde se encuentra nuestro sistema solar y por ende el lugar en donde habitamos.'
+      }),
       this.milkyway
     );
+    ////////////////////HASTA AQUI/////////////////////////
 
     r360.detachRoot(welcomePanelTag)
   }
+
+  resizePanel(width, height, id){
+    switch (id) {
+      case 'milkyway':
+        this.milkyway.resize(width,height)
+        break;
+      default:
+        this.milkyway.resize(width,height)
+        break;
+    }
+  }
+  
 }
 
 window.React360 = {init};
--- src/infoPanel.js
+++ src/infoPanel.js
@@ -4,8 +4,12 @@ import {
   Text,
   View,
   Image,
-  asset
+  asset,
+  NativeModules
 } from 'react-360';
+
+const appModule = NativeModules.appModule
+
 
 export default class InfoPanel extends React.Component {
   constructor(props){
@@ -19,13 +23,44 @@ export default class InfoPanel extends React.Component {
     }
   }
 
+  modifyPanel = (id) => {
+    appModule.resizePanel(500,700,id)
+    this.setState({
+        ...this.state,
+        image: {
+            ...this.state.image,
+            src: `${id}.jpg`,
+            width: 500,
+            height: 500 
+        }
+    })
+  }
+
+  resetPanel = (id) => {
+    appModule.resizePanel(50,50,id)
+    this.setState({
+        ...this.state,
+        image: {
+            ...this.state.image,
+            src: `${id}.png`,
+            width: 50,
+            height: 50 
+        }
+    })
+  }
+
   render() {
     const {image} = this.state;
+    const {id, text} = this.props;
 
     return (
-      <View style={styles.panel}>
+      <View style={styles.panel} onEnter={() => this.modifyPanel(id)} onExit={() => this.resetPanel('eye')} hitSlop={500}>
         <Image source={asset(image.src)} style={{width: image.width, height: image.height}}/>
-        
+        <View style={styles.infoBox}>
+            <Text style={styles.info}>
+              {text}
+            </Text>
+        </View>
       </View>
     );
   }
@@ -34,9 +69,18 @@ export default class InfoPanel extends React.Component {
 const styles = StyleSheet.create({
   panel: {
     // Fill the entire surface
-    width: 100,
-    height: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: 50,
+    height: 50,
+  },
+  infoBox: {
+    padding: 20,
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderWidth: 2,
+    width: 500
+  },
+  info: {
+    fontSize: 30,
+    color: 'black',
   },
 });
